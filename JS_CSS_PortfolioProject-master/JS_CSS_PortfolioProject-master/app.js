66139b17c0167d33562ddb802d944f8c0034f7e9
LucMartin91 <105196759+LucMartin91@users.noreply.github.com>
2023-08-27 21:20:50 +0200

    Add files via upload

--- JS_CSS_PortfolioProject-master/JS_CSS_PortfolioProject-master/app.js
+++ JS_CSS_PortfolioProject-master/JS_CSS_PortfolioProject-master/app.js
@@ -1,6 +1,6 @@
 (function () {
     [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
             document.querySelector(".active-btn").classList.remove("active-btn");
             this.classList.add("active-btn");
             document.querySelector(".active").classList.remove("active");
@@ -11,3 +11,41 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+document.addEventListener("DOMContentLoaded", function () {
+    const controls = document.querySelectorAll(".control");
+    const titleDisplay = document.createElement("h2");
+    titleDisplay.style.position = "fixed";
+    titleDisplay.style.top = "10px";
+    titleDisplay.style.left = "50%";
+    titleDisplay.style.transform = "translateX(-50%)";
+    titleDisplay.style.opacity = "0";
+    titleDisplay.style.transition = "opacity 0.5s";
+    document.body.appendChild(titleDisplay);
+
+    controls.forEach(control => {
+        control.addEventListener("mouseover", function () {
+            const sectionId = control.getAttribute("data-id");
+            let titleText = "";
+            switch (sectionId) {
+                case "about":
+                    titleText = "A propos de moi";
+                    break;
+                case "portfolio":
+                    titleText = "Mon Portfolio";
+                    break;
+                case "blogs":
+                    titleText = "Mes Blogs";
+                    break;
+                case "contact":
+                    titleText = "Contactez-moi";
+                    break;
+            }
+            titleDisplay.textContent = "Aller à : " + titleText;
+            titleDisplay.style.opacity = "1";
+        });
+
+        control.addEventListener("mouseout", function () {
+            titleDisplay.style.opacity = "0";
+        });
+    });
+});
